c9dc58bf77f272b6981fc62fbfd29701e533e490
Steven Basher <sbasher314@gmail.com>
2021-05-20 22:30:17 -0400

    Refactored port #s

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3000;
 const path = require('path');
 const request = require('request');
 
 app.use(express.static(path.join(__dirname, '/public')));
-
-/*app.get('/', (req, res) => {
-  res.redirect('./dp/1');
-});*/
 
 const sendIndex = (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
@@ -17,18 +13,6 @@ const sendIndex = (req, res) => {
 app.get('/:productId', sendIndex);
 
 app.get('*/dp/:productId', sendIndex);
-
-app.get('/images/:productId', (req, res) => {
-  request('http://localhost:1166' + req.url, function (err, response, body) {
-    res.json(JSON.parse(body));
-  });
-});
-
-app.get('/Information', (req, res) => {
-  request('http://localhost:3001' + req.url, function (err, response, body) {
-    res.json(JSON.parse(body));
-  });
-});
 
 app.listen(port, () => {
   console.log(`Product Gallery listening at http://localhost:${port}`);
