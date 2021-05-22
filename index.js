bf595a031aeb3af5d5d180e70d425e88ef40f63c
Steven Basher <sbasher314@gmail.com>
2021-05-22 16:37:16 -0400

    More proxy stuff

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const sendIndex = (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 };
 
+app.get('/user_image.png', (req, res) => {
+  request('http://localhost:3004/user_image.png').pipe(res);
+});
+
+
+
 app.get('/:productId', sendIndex);
 
 app.get('*/dp/:productId', sendIndex);
